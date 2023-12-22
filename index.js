5feec74cc6e6e74ae10ec9ddb0be51ace112b14a
lil Wolf <59448133+lilWolf011@users.noreply.github.com>
2023-12-22 17:48:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require("path");
 const qrcode = require("qrcode-terminal");
 const { Client, NoAuth } = require("whatsapp-web.js");
 
+//If you want to connect this app to the discord webhook, remove the comment lines below.
+
 //const axios = require("axios");
 /*
 async function postMessageToWebhook(embeds) {
@@ -29,11 +31,9 @@ async function postMessageToWebhook(embeds) {
 */
 const client = new Client({
   authStrategy: new NoAuth(),
-  puppeteer: {
-		args: ['--no-sandbox'],
-	}
 });
 
+// qr code in terminal
 client.on("qr", (qr) => {
   console.log("QR RECEIVED", qr);
   qrcode.generate(qr, { small: true });
@@ -43,91 +43,97 @@ client.on("ready", () => {
   console.log("Client is ready!");
 });
 
+//detecting that a message has arrived
 client.on("message", async (msg) => {
   const date = new Date().toISOString().substring(0, 10);
   const time = new Date();
   const formattedTime = time
     .toISOString()
     .replace(/T/, " ")
+    .replace(/:/g, "_")
     .split(" ")[1]
-    .split(".")[0];
-  const fullDate = date + "_" + formattedTime;
-  console.log(msg['_data']);
+    .split(".")[0]; //time: 17_57_28
+  //console.log(msg['_data']); //remove the comment line if you want to see all the information of incoming messages in the console.
   const person = msg._data.notifyName;
   var phoneNumber;
+  //to understand if messages come from the group or from a dm
   if (msg.id.participant !== undefined) {
-    // 'participant' alanı tanımlı ise GRUPTUR
     var phoneNumber = msg._data.author.replace(/@c.us/g, "");
   } else {
-    // 'participant' alanı tanımlı değilse DM'DİR
     var phoneNumber = msg._data.from.replace(/@c.us/g, "");
   }
-  
 
   if (msg.hasMedia) {
     const media = await msg.downloadMedia();
 
     if (msg.type === "ptt") {
+      // message type === voice message
       try {
         console.log("Voice Clip Received");
-        const formattedTimeWithUnderscore = formattedTime.replace(/:/g, "_");
-        const folder = path.join(process.cwd(), "audio", `${phoneNumber}_${person}`, date);
+        const folder = path.join(
+          process.cwd(),
+          "audio",
+          `${phoneNumber}_${person}`,
+          date
+        );
         await mkdir(folder, { recursive: true });
-    
-        const filename = path.join(folder, formattedTimeWithUnderscore + '.ogg');
 
-        //your code to be executed after 1 second
-        const decodedData = Buffer.from(media.data, 'base64');
-        fs.writeFileSync(filename, decodedData);
+        const filename = path.join(folder, `${formattedTime} ${msg.id.id}.ogg`);
+
+        const decodedData = Buffer.from(media.data, "base64");
+        writeFile(filename, decodedData);
         console.log("Voice clip saved successfully:", filename);
-        //await writeFile(filename, media.data, "binary");
-      
       } catch (error) {
         console.error("Error saving voice clip:", error);
       }
-    }
-    
-    else if (msg.type === "image") {
+    } else if (msg.type === "image") {
+      // message type === image
       try {
-        const folder = path.join(process.cwd(), "img", `${phoneNumber}_${person}`, date);
+        const folder = path.join(
+          process.cwd(),
+          "img",
+          `${phoneNumber}_${person}`,
+          date
+        );
         await mkdir(folder, { recursive: true });
-    
-        const filename = path.join(folder, `${formattedTime}_${msg.id.id}.${media.mimetype.split("/")[1]}`);
-        await writeFile(filename, Buffer.from(media.data, "base64"), "binary");
-    
+
+        const filename = path.join(
+          folder,
+          `${formattedTime} ${msg.id.id}.${media.mimetype.split("/")[1]}`
+        );
+        writeFile(filename, Buffer.from(media.data, "base64"), "binary");
+
         console.log("Image saved successfully:", filename);
       } catch (error) {
         console.error("Error saving image:", error);
       }
-    }    
-    //else if (msg.type == "") {}
-  } 
-  
-  
-  else if (msg.type === "chat") {
-    const message = msg.body;
+    }
+    //else if (msg.type == "PDF") {} // Will be added soon (Maybe)
+  } else if (msg.type === "chat") {
     const json = [
       {
         Id: msg.id.id,
         Number: phoneNumber,
         Person: person,
-        Date: fullDate,
-        Message: message,
+        Date: date,
+        Time: formattedTime.replace(/_/g, ":"),
+        Message: msg.body,
       },
     ];
 
     const dir = "./text";
-    await mkdir(dir, { recursive: true });
+    mkdir(dir, { recursive: true });
 
     const filePath = path.join(__dirname, dir, "text.json");
 
     try {
-      const data = JSON.parse(await readFile(filePath, "utf8"));
-      const jsonArray = [...data, ...json];
-      await writeFile(filePath, JSON.stringify(jsonArray));
+      const data = JSON.parse(await readFile(filePath, "utf8")); // Read the current data in the file
+      const jsonArray = [...data, ...json]; // Merge existing data with new data
+      writeFile(filePath, JSON.stringify(jsonArray)); // Write merged data to file
     } catch (error) {
-      await writeFile(filePath, JSON.stringify(json));
+      console.error("Error saving message: ", error);
     }
+    // if you want the discord webhook to work you need to remove the comment line here
     /*
     let embeds = [
       {
@@ -156,7 +162,7 @@ client.on("message", async (msg) => {
           }
         ],
         footer: {
-          text: `Date • ${fullDate}`
+          text: `Date • ${date} & Time • ${formattedTime.replace(/_/g, ":")}`
         },
       },
     ];
