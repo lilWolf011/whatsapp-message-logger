2d3d5c61b1f2aabb3a5e9c6a5ff937984f4e4bd5
lil Wolf <59448133+lilWolf011@users.noreply.github.com>
2023-12-23 14:09:46 +0300

    Update index.js
    Can now record videos and images and send recorded images and videos to Discord via webhook (for this, you need to remove the comment lines in the code and add your webhook URL). Many bugs have been fixed, but there may still be bugs, so please report if you encounter any.

--- index.js
+++ index.js
@@ -10,28 +10,28 @@ async function postMessageToWebhook(embeds) {
   let data = JSON.stringify({ embeds });
   let config = {
     method: "POST",
-    url: 'WEBHOOK URL HERE', // url: 'https://discord.com/webhook/url/here ',
+    url: "Your webhook url here", // url: 'https://discord.com/webhook/url/here ',
     headers: { "Content-Type": "application/json" },
     data: data,
- };
+  };
 
- //Send the request
- axios(config)
-   .then((response) => {
+  //Send the request
+  axios(config)
+    .then((response) => {
       console.log("Webhook delivered successfully");
       return response;
-   })
-   .catch((error) => {
-     console.log(error);
-     return error;
-   });
+    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
 }
 */
 const client = new Client({
   authStrategy: new NoAuth(),
   puppeteer: {
-   args: ['--no-sandbox'],
-  }
+    args: ["--no-sandbox"],
+  },
 });
 
 client.on("qr", (qr) => {
@@ -53,7 +53,7 @@ client.on("message", async (msg) => {
     .split(" ")[1]
     .split(".")[0];
   const fullDate = date + "_" + formattedTime;
-  //console.log(msg['_data']);
+  console.log(msg["_data"]);
   const person = msg._data.notifyName;
   var phoneNumber;
   if (msg.id.participant !== undefined) {
@@ -63,47 +63,55 @@ client.on("message", async (msg) => {
     // 'participant' alanı tanımlı değilse DM'DİR
     var phoneNumber = msg._data.from.replace(/@c.us/g, "");
   }
-  
 
   if (msg.hasMedia) {
     const media = await msg.downloadMedia();
 
     if (msg.type === "ptt") {
       try {
-        console.log("Voice Clip Received");
-        
-        const folder = path.join(process.cwd(), "audio", `${phoneNumber}_${person}`, date);
+        const folder = path.join(
+          process.cwd(),
+          "audio",
+          `${phoneNumber}_${person}`,
+          date,
+        );
         await mkdir(folder, { recursive: true });
-    
-        const filename = path.join(folder, `${formattedTime}.ogg`);
+        const filename = path.join(
+          folder,
+          `${formattedTime}_${msg.id.id}.${media.mimetype.split("/")[1]}`,
+        );
 
-        const decodedData = Buffer.from(media.data, 'base64');
+        const decodedData = Buffer.from(media.data, "base64");
         fs.writeFileSync(filename, decodedData);
         console.log("Voice clip saved successfully:", filename);
-        //await writeFile(filename, media.data, "binary");
       } catch (error) {
         console.error("Error saving voice clip:", error);
       }
-    }
-    
-    else if (msg.type === "image") {
+    } else if (msg.type === "image" || msg.type === "video") {
       try {
-        const folder = path.join(process.cwd(), "img", `${phoneNumber}_${person}`, date);
+        const folder = path.join(
+          process.cwd(),
+          media.mimetype.split("/")[0],
+          `${phoneNumber}_${person}`,
+          date,
+        );
         await mkdir(folder, { recursive: true });
-    
-        const filename = path.join(folder, `${formattedTime}_${msg.id.id}.${media.mimetype.split("/")[1]}`);
+        const filename = path.join(
+          folder,
+          `${formattedTime} ${msg.id.id}.${media.mimetype.split("/")[1]}`,
+        );
+
         await writeFile(filename, Buffer.from(media.data, "base64"), "binary");
-    
-        console.log("Image saved successfully:", filename);
+        console.log(
+          `${media.mimetype.split("/")[0]} saved successfully:`,
+          filename,
+        );
       } catch (error) {
-        console.error("Error saving image:", error);
+        console.error(`Error saving ${media.mimetype.split("/")[0]}:`, error);
       }
-    }    
+    }
     //else if (msg.type == "") {}
-  } 
-  
-  
-  else if (msg.type === "chat") {
+  } else if (msg.type === "chat") {
     const message = msg.body;
     const json = [
       {
@@ -118,7 +126,6 @@ client.on("message", async (msg) => {
 
     const dir = "./text";
     await mkdir(dir, { recursive: true });
-
     const filePath = path.join(__dirname, dir, "text.json");
 
     try {
@@ -137,26 +144,26 @@ client.on("message", async (msg) => {
           {
             name: "Id",
             value: `${msg.id.id}`,
-            inline: true
+            inline: true,
           },
           {
             name: "Phone Number",
             value: `${phoneNumber}`,
-            inline: true
+            inline: true,
           },
           {
             name: "Person",
             value: `${person}`,
-            inline: true
+            inline: true,
           },
           {
             name: "Message",
             value: `${message}`,
-            inline: true
-          }
+            inline: true,
+          },
         ],
         footer: {
-          text: `Date • ${fullDate}`
+          text: `Date • ${fullDate}`,
         },
       },
     ];
