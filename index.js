12677655c5af54b33b7a09fc7efb8e8f9f728258
lil Wolf <59448133+lilWolf011@users.noreply.github.com>
2024-01-20 00:46:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,15 +9,35 @@ const client = new Client({
     args: ["--no-sandbox"],
   },
 });
-
 client.on("qr", (qr) => {
   console.log("QR RECEIVED", qr);
   qrcode.generate(qr, { small: true });
 });
-
 client.on("ready", () => {
   console.log("Client is ready!");
 });
+
+/*
+const axios = require("axios");
+async function postMessageToWebhook(embeds) {
+  let data = JSON.stringify({ embeds });
+    let config = {
+    method: "POST",
+    url: "your webhook url", // url: 'https://discord.com/webhook/url/here ',
+    headers: { "Content-Type": "application/json" },
+    data: data,
+  };
+  axios(config)
+    .then((response) => {
+      console.log("Webhook delivered successfully");
+      return response;
+    })
+      .catch((error) => {
+      console.log(error);
+      return error;
+    });
+}
+*/
 
 client.on("message", async (msg) => {
   const date = new Date().toISOString().substring(0, 10);
@@ -30,6 +50,8 @@ client.on("message", async (msg) => {
     .split(".")[0];
   //console.log(msg["_data"]);
   const person = msg._data.notifyName;
+  const fullDate = date + " " + formattedTime.replace(/_/g, ":");
+
   var phoneNumber;
   if (msg.id.participant !== undefined) {
     // 'participant' alanı tanımlı ise GRUPTUR
@@ -38,54 +60,60 @@ client.on("message", async (msg) => {
     // 'participant' alanı tanımlı değilse DM'DİR
     var phoneNumber = msg._data.from.replace(/@c.us/g, "");
   }
-
+/*
+  let embeds = [
+    {
+      title: "Whatsapp Message",
+      color: 5814783,
+      fields: [
+        {
+          name: "Id",
+          value: `${msg.id.id}`,
+          inline: true,
+        },
+        {
+          name: "Phone Number",
+          value: `${phoneNumber}`,
+          inline: true,
+        },
+        {
+          name: "Person",
+          value: `${person}`,
+          inline: true,
+        },
+        {
+          name: "Message",
+          value: "undefined",
+          inline: true,
+        },
+      ],
+      footer: {
+        text: `Date • ${fullDate}`,
+      },
+    },
+  ]
+*/
   if (msg.hasMedia) {
     const media = await msg.downloadMedia();
-
-    if (msg.type === "ptt") {
+    if (msg.type === "image" || msg.type === "video" || msg.type === "ptt") {
       try {
-        const folder = path.join(
-          process.cwd(),
-          "audio",
-          `${phoneNumber}_${person}`,
-          date,
-        );
-        fs.mkdirSync(folder, { recursive: true });
-        const filename = path.join(
-          folder,
-          `${formattedTime}_${msg.id.id}.${media.mimetype.split("/")[1]}`,
-        );
+        const folder = path.join(process.cwd(), media.mimetype.split("/")[0], `${phoneNumber}_${person}`, date,);
 
-        const decodedData = Buffer.from(media.data, "base64");
-        fs.writeFileSync(filename, decodedData);
-        console.log("Voice clip saved successfully:", filename);
-      } catch (error) {
-        console.error("Error saving voice clip:", error);
-      }
-    } else if (msg.type === "image" || msg.type === "video") {
-      try {
-        const folder = path.join(
-          process.cwd(),
-          media.mimetype.split("/")[0],
-          `${phoneNumber}_${person}`,
-          date,
-        );
         fs.mkdirSync(folder, { recursive: true });
-        const filename = path.join(
-          folder,
-          `${formattedTime} ${msg.id.id}.${media.mimetype.split("/")[1]}`,
-        );
 
+        let splited = media.mimetype.split("/")['1'];
+        const filename = path.join(folder,`${formattedTime} ${msg.id.id}.${msg.type === "ptt" ? (splited.split(";")[0]) : splited}`,);
+    
         fs.writeFileSync(filename, Buffer.from(media.data, "base64"), "binary");
-        console.log(
-          `${media.mimetype.split("/")[0]} saved successfully:`,
-          filename,
-        );
+
+        console.log(`${media.mimetype.split("/")[0]} saved successfully:`, filename,);
+
+        //embeds[0].fields[3].value = filename;
+        //postMessageToWebhook(embeds);
       } catch (error) {
         console.error(`Error saving ${media.mimetype.split("/")[0]}:`, error);
       }
     }
-    //else if (msg.type == "") {}
   } else if (msg.type === "chat") {
     const message = msg.body;
     const json = [
@@ -108,65 +136,12 @@ client.on("message", async (msg) => {
       const jsonArray = [...data, ...json];
       fs.writeFileSync(filePath, JSON.stringify(jsonArray));
     } catch (error) {
-      console.log("error yazıo");
+      console.log("error yazıo")
       console.log(error);
       fs.writeFileSync(filePath, JSON.stringify(json));
     }
-
-    /* Send the message to discord
-    const axios = require("axios");
-    async function postMessageToWebhook(embeds) {
-      let data = JSON.stringify({ embeds });
-        let config = {
-        method: "POST",
-        url: "Discord Webhook URL", // url: 'https://discord.com/webhook/url/here ',
-        headers: { "Content-Type": "application/json" },
-        data: data,
-      };
-       axios(config)
-        .then((response) => {
-          console.log("Webhook delivered successfully");
-          return response;
-        })
-          .catch((error) => {
-          console.log(error);
-          return error;
-        });
-    }
-    const fullDate = date + " " + formattedTime.replace(/_/g, ":");
-    let embeds = [
-      {
-        title: "Whatsapp Message",
-        color: 5814783,
-        fields: [
-          {
-            name: "Id",
-            value: `${msg.id.id}`,
-            inline: true,
-          },
-          {
-            name: "Phone Number",
-            value: `${phoneNumber}`,
-            inline: true,
-          },
-          {
-            name: "Person",
-            value: `${person}`,
-            inline: true,
-          },
-          {
-            name: "Message",
-            value: `${message}`,
-            inline: true,
-          },
-        ],
-        footer: {
-          text: `Date • ${fullDate}`,
-        },
-      },
-    ];
-    postMessageToWebhook(embeds);
-    */
+    //embeds[0].fields[3].value = message;
+    //postMessageToWebhook(embeds);
   }
 });
 
